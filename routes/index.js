fd79171ce2357a8360dc064a2402790692d80b40
Maxim Filimonov <tpaktopsp@gmail.com>
2012-10-19 12:02:30 +1100

    Add multiple techologies and different rings

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
-exports.about = function(req,res){
-  res.render('about', { title: 'About' });
-};
-exports.contact = function(req,res) {
-  res.render('contact', { title: 'Contact' });
+  res.render('index', { title: 'Express', technologies: ['Javascript', 'Node', 'Mobile'] });
 };
